50dab14ffdde6e4f319486e5f19ab45ed89091ec
Adeline <adelinesun@gmail.com>
2019-07-09 14:59:01 -0700

    Initial Commit

---
+++ Javascript.js
@@ -0,0 +1,21 @@
+alert("Welcome to the Repertoire Directory!");
+function bigImg(x){
+  x.style.height = "350px";
+  x.style.width = "350px";
+}
+function oriImg(x){
+  x.style.height = "300px";
+  x.style.width = "300px";
+}
+
+function textColor(x){
+  x.style.color = "grey"
+}
+
+function oriColor(x){
+  x.style.color = "white"
+}
+
+function sound(x){
+  x.play();
+}
